8f965abd7b329c1312124a252008ccfc28129190
Maryammmy <maryamsameh1610@gmail.com>
2025-07-29 20:09:24 +0300

    height

--- components/main/subscription/subscription-plans/index.tsx
+++ components/main/subscription/subscription-plans/index.tsx
@@ -14,7 +14,7 @@ function SubscriptionPlans() {
       title={<Header />}
       contentClassName="max-w-full lg:max-w-[900px]"
     >
-      <div className="pt-1 sm:pt-6 space-y-5 sm:space-y-10 max-h-[calc(95vh-144px)] sm:max-h-[70vh] overflow-y-auto">
+      <div className="pt-1 sm:pt-6 space-y-5 sm:space-y-10 max-h-[calc(90vh-144px)] sm:max-h-[70vh] overflow-y-auto">
         <PeriodFilters />
         <SubscriptionPlanList />
       </div>
