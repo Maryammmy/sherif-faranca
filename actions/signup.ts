0d02daae8b114f3214922128d273c0637db850ac
Maryammmy <maryamsameh1610@gmail.com>
2025-07-31 23:28:04 +0300

    sign up

--- actions/signup.ts
+++ actions/signup.ts
@@ -24,7 +24,7 @@ export async function signupAction(
   try {
     const response = await sendRegistrationAPI(parsed.data);
     console.log("API response:", response?.data?.message);
-    return { success: true, errors: {}, message: "OTP sent successfully" };
+    return { success: true, errors: {}, message: response?.data?.message };
   } catch (error) {
     console.error("API error:", error);
     return { success: false, errors: {}, message: "Failed to send email" };
