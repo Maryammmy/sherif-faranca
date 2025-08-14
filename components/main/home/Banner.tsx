6af3a9c8f10eec4fad294d5cf1910f0eb529984a
Maryammmy <maryamsameh1610@gmail.com>
2025-08-14 17:43:20 +0300

    solve image

--- components/main/home/Banner.tsx
+++ components/main/home/Banner.tsx
@@ -7,7 +7,9 @@ import Image from "next/image";
 export default function Banner() {
   const { data } = useHome();
   const merchantBanner: IMerchantBanner = data?.data?.merchantBanners;
+  if (!data) return null;
   const { imageUrl, title } = merchantBanner;
+
   return (
     <div className="relative w-full h-[400px] rounded-3xl overflow-hidden mb-5">
       <Image
