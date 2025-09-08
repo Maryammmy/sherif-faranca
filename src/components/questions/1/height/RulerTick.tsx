64210cebf0067f4a7d439cd5871b1ff50bf60a03
Maryammmy <maryamsameh1610@gmail.com>
2025-09-08 16:30:05 +0300

    ruler tooltip

--- src/components/questions/1/height/RulerTick.tsx
+++ src/components/questions/1/height/RulerTick.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/src/lib/utils";
 import { Button } from "@/src/components/ui/Button";
+import { useState } from "react";
 
 interface IProps {
   value: number;
@@ -14,34 +15,45 @@ export default function RulerTick({
   shouldHighlight,
   onClick,
 }: IProps) {
+  const [hovered, setHovered] = useState(false);
   return (
-    <Button
-      className="flex flex-col items-center cursor-pointer"
-      style={{ width: "20px" }}
-      onClick={onClick}
-    >
-      <div
-        className={cn(
-          "w-[2px]",
-          isMajor
-            ? shouldHighlight
-              ? "h-10 bg-primary"
-              : "h-10 bg-black"
-            : shouldHighlight
-            ? "h-6 bg-primary/70"
-            : "h-6 bg-gray-400"
-        )}
-      />
-      {isMajor && (
-        <span
+    <div className="relative flex flex-col items-center">
+      <Button
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className="flex flex-col items-center cursor-pointer"
+        style={{ width: "20px" }}
+        onClick={onClick}
+      >
+        <div
           className={cn(
-            "text-sm mt-1 font-bold",
-            shouldHighlight ? "text-primary" : "text-gray-700"
+            "w-[2px]",
+            isMajor
+              ? shouldHighlight
+                ? "h-10 bg-primary"
+                : "h-10 bg-black"
+              : shouldHighlight
+              ? "h-6 bg-primary/70"
+              : "h-6 bg-gray-400"
           )}
-        >
+        />
+        {isMajor && (
+          <span
+            className={cn(
+              "text-sm mt-1 font-bold",
+              shouldHighlight ? "text-primary" : "text-gray-700"
+            )}
+          >
+            {value}
+          </span>
+        )}
+      </Button>
+      {/* Custom Tooltip */}
+      {hovered && (
+        <span className="absolute z-10 bottom-full mb-2 px-2 py-1 rounded bg-primary text-white text-xs font-medium whitespace-nowrap">
           {value}
         </span>
       )}
-    </Button>
+    </div>
   );
 }
