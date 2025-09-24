56bc29d674ab75ec268e10264033674b05f666d9
Maryammmy <maryamsameh1610@gmail.com>
2025-09-24 18:32:34 +0300

    email

--- src/components/auth/signin/signin-email/SigninEmailForm.tsx
+++ src/components/auth/signin/signin-email/SigninEmailForm.tsx
@@ -35,11 +35,9 @@ function SignInEmailForm() {
       }
       toast.success(response?.message);
       setTimeout(() => {
-        if (isAnswared) {
-          router.push("/");
-        } else {
-          router.push("/questions/1");
-        }
+        const path = isAnswared ? "/" : "/questions/1";
+        router.push(path);
+        router.refresh();
       }, 500);
     } else {
       toast.error(response?.message);
